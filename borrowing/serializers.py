e3965fce08c9046bdf83b5be285aebab33871113
Vitalii <vetali5700gmail.com>
2024-09-16 21:26:02 +0300

    add borrowing list & detail endpoint

---
+++ borrowing/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+
+from book.serializers import BookSerializer
+from borrowing.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book = serializers.CharField(read_only=True, source="book.__str__")
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BookSerializer(many=False)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingDetailSerializer,
+    BorrowingListSerializer,
+)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        if self.action == "list":
+            return BorrowingListSerializer
+        return BorrowingSerializer
