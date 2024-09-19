68e5db102571243f9fec55b4436cae1a59a8058c
Vitalii <vetali5700gmail.com>
2024-09-19 22:34:31 +0300

    simplify borrowing list and retrieve serializers to general BorrowingSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -5,20 +5,8 @@ from borrowing.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Borrowing
-        fields = (
-            "id",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_date",
-            "book",
-            "user",
-        )
-
-
-class BorrowingListSerializer(serializers.ModelSerializer):
-    book = serializers.CharField(read_only=True, source="book.__str__")
+    book = BookSerializer(read_only=True)
+    user = serializers.CharField(read_only=True, source="user.email")
 
     class Meta:
         model = Borrowing
@@ -30,24 +18,10 @@ class BorrowingListSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+        read_only_fields = ("actual_return_date",)
 
 
-class BorrowingDetailSerializer(BorrowingSerializer):
-    book = BookSerializer(many=False)
-
-    class Meta:
-        model = Borrowing
-        fields = (
-            "id",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_date",
-            "book",
-            "user",
-        )
-
-
-class BorrowingCreateSerializer(BorrowingSerializer):
+class BorrowingCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = (
