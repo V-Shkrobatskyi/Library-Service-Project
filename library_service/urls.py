898773cbd9ae72f3b49859e95945d4ca6837e061
Vitalii <vetali5700gmail.com>
2024-09-19 22:28:32 +0300

    fix path names to: users, books, borrowings

--- library_service/urls.py
+++ library_service/urls.py
@@ -21,9 +21,9 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "api/",
+        "api/books/",
         include("book.urls", namespace="book"),
     ),
     path("api/users/", include("user.urls", namespace="user")),
-    path("api/", include("borrowing.urls", namespace="borrowing")),
+    path("api/borrowings/", include("borrowing.urls", namespace="borrowing")),
 ]
