ac8572839e2983277d6e2cf59a9465c7b6a876a2
Vitalii <vetali5700gmail.com>
2024-09-28 23:25:09 +0300

    add notification for borrowings overdue

---
+++ borrowing/borrowing_overdue.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from borrowing.models import Borrowing
+from borrowing.helpers.telegram import send_message
+
+
+def check_borrowings_overdue() -> None | str:
+    today = datetime.today().date()
+    borrowings_overdue = Borrowing.objects.filter(
+        expected_return_date__lte=today,
+        actual_return_date__isnull=True,
+    )
+
+    if borrowings_overdue:
+        for borrowing in borrowings_overdue:
+            message = (
+                f"Borrowing overdue:\n"
+                f"borrowing id: {borrowing.id}\n"
+                f"book: {borrowing.book.title}\n"
+                f"user: {borrowing.user}\n"
+                f"borrow_date: {borrowing.borrow_date}\n"
+                f"expected_return_date: {borrowing.expected_return_date}"
+            )
+
+            send_message(message)
+
+        message = f"{len(borrowings_overdue)} total borrowings overdue today."
+        send_message(message)
+
+    else:
+        message = "No borrowings overdue today!"
+        send_message(message)
+
+    return message
