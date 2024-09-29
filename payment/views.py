7bda21986c883298c8bd9f93e6411af228608037
Vitalii <vetali5700gmail.com>
2024-09-29 23:11:25 +0300

    add PaymentViewSet

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import (
+    PaymentSerializer,
+    PaymentDetailSerializer,
+)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.select_related("borrowing")
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if not self.request.user.is_staff:
+            return self.queryset.filter(borrowing__user=user)
+
+        return self.queryset
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+
+        return PaymentSerializer
