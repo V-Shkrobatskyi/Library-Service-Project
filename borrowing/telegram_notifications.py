8c8c4260b45de884edbc0ae5bb1806dbf1959eb4
Vitalii <vetali5700gmail.com>
2024-10-03 21:19:23 +0300

    fix move API_URL from send_message function to external variable

--- borrowing/telegram_notifications.py
+++ borrowing/telegram_notifications.py
@@ -7,10 +7,11 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+API_URL = "https://api.telegram.org/bot"
 
 
 def send_message(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = f"{API_URL}{TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
 
     response = requests.post(url, data=data)
