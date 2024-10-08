293488d4267839207449c3cc9e78de0f95f77d5a
Vitalii <vetali5700gmail.com>
2024-10-08 22:34:20 +0300

    add checks for success payment endpoint and error message for cancel endpoint

--- payment/views.py
+++ payment/views.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets
+import stripe
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from payment.models import Payment
 from payment.serializers import (
     PaymentSerializer,
     PaymentDetailSerializer,
+    PaymentSuccessSerializer,
 )
 
 
@@ -22,15 +27,50 @@ class PaymentViewSet(viewsets.ModelViewSet):
         return self.queryset
 
     def get_serializer_class(self):
-        if self.action in ("retrieve", "success"):
+        if self.action == "retrieve":
             return PaymentDetailSerializer
+        if self.action == "success":
+            return PaymentSuccessSerializer
 
         return PaymentSerializer
 
     @action(methods=["GET"], url_path="success", detail=False)
     def success(self, request, session_id=None):
-        pass
+        session_id = request.query_params.get("session_id", None)
+        if not session_id:
+            return Response(
+                {"message": "Payment parameter not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        payment = get_object_or_404(
+            Payment,
+            session_id=request.GET.get("session_id"),
+            status=Payment.StatusChoices.PENDING,
+            type=Payment.TypeChoices.PAYMENT,
+        )
+        session = stripe.checkout.Session.retrieve(session_id)
+
+        if session["status"] == "complete":
+            data = {
+                "status": Payment.StatusChoices.PAID,
+                "type": Payment.TypeChoices.PAYMENT,
+            }
+            serializer = self.get_serializer(payment, data=data)
+            borrowing = payment.borrowing
+
+            if serializer.is_valid():
+                serializer.save()
+                borrowing.return_book()
+
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     @action(methods=["GET"], url_path="cancel", detail=False)
     def cancel(self, request):
-        pass
+        raise ValidationError(
+            "Payment can be paid a bit later (but the session is available for only 24h)"
+        )
