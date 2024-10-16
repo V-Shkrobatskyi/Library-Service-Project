68303523ca321eca68bb17864f12b32feb6642d0
Vitalii <vetali5700gmail.com>
2024-10-16 23:08:40 +0300

    add drf-spectacular endpoints

--- library_service/urls.py
+++ library_service/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +32,13 @@ urlpatterns = [
     path("api/users/", include("user.urls", namespace="user")),
     path("api/borrowings/", include("borrowing.urls", namespace="borrowing")),
     path("api/payments/", include("payment.urls", namespace="payment")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
