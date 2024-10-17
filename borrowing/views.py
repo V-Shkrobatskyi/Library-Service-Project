887ac73ac911c2a0ad6c38f7995ae09a74b33ecf
Vitalii <vetali5700gmail.com>
2024-10-17 22:17:08 +0300

    add descriptions to documentation for endpoints methods

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,6 @@
 from django.db.transaction import atomic
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -44,10 +46,17 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
         return queryset
 
+    @extend_schema(
+        description="Create new borrowing." "Validate no pending and expired payments.",
+    )
     def perform_create(self, serializer):
         user = self.request.user
         serializer.save(user=user)
 
+    @extend_schema(
+        description="Return borrowing by add actual_return_date."
+        "Check borrowing overdue and if exist add overdue payments.",
+    )
     @action(
         detail=True,
         methods=["POST"],
@@ -65,3 +74,24 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="user_id",
+                type=OpenApiTypes.STR,
+                description="Filter by user ID (ex. ?user_id=10). For admin only.",
+                required=False,
+            ),
+            OpenApiParameter(
+                name="is_active",
+                type=OpenApiTypes.STR,
+                description="Filter by active/returned borrowing status."
+                "(ex. ?is_active=true for active "
+                "and ?is_active=false for returned borrowings)",
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
